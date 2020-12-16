623eecef3c7f7aeb3ca81fa507922b64fc32d32c
ckchamara <ckchamara22@gmail.com>
2020-12-17 00:34:31 +0530

    initial

--- Board/Task/src/board/BoardImpl.kt
+++ Board/Task/src/board/BoardImpl.kt
@@ -8,6 +8,7 @@ open class SquareBoardImpl(override val width: Int) : SquareBoard {
 
     private val arrayList = ArrayList<Cell>(width * width)
 
+
     init {
         (1..width).forEach { it ->
             (1..width).forEach { itr ->
@@ -16,6 +17,7 @@ open class SquareBoardImpl(override val width: Int) : SquareBoard {
         }
     }
 
+
     override fun getCellOrNull(i: Int, j: Int): Cell? = getAllCells().find { it -> it == Cell(i, j) }
 
 
@@ -23,17 +25,17 @@ open class SquareBoardImpl(override val width: Int) : SquareBoard {
         return getAllCells().find { it -> it == Cell(i, j) } ?: throw IllegalArgumentException("Invalid Cell ($i, $j)")
     }
 
-    override fun getAllCells(): Collection<Cell> {
-        return arrayList
-    }
 
-    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
-        return jRange.filter { it -> it <= width }.map { itr -> getCell(i, itr) }
-    }
+    override fun getAllCells(): Collection<Cell> = arrayList
 
-    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
-        return iRange.filter { it -> it <= width }.map { itr -> getCell(itr, j) }
-    }
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> =
+            jRange.filter { it <= width }.map { itr -> getCell(i, itr) }
+
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> =
+            iRange.filter { it <= width }.map { itr -> getCell(itr, j) }
+
 
     override fun Cell.getNeighbour(direction: Direction): Cell? {
         return when (direction) {
@@ -46,30 +48,27 @@ open class SquareBoardImpl(override val width: Int) : SquareBoard {
 
 }
 
-class GameBoardImpl<T>(width: Int): GameBoard<T>, SquareBoardImpl(width) {
+class GameBoardImpl<T>(width: Int) : GameBoard<T>, SquareBoardImpl(width) {
 
     private val boardCells = getAllCells().map { it to null as T? }.toMap().toMutableMap()
 
+
     override fun get(cell: Cell): T? = boardCells[cell]
+
 
     override fun set(cell: Cell, value: T?) {
         boardCells[cell] = value
     }
 
-    override fun filter(predicate: (T?) -> Boolean): Collection<Cell> {
-            return boardCells.filterValues(predicate).keys
-    }
+    override fun filter(predicate: (T?) -> Boolean): Collection<Cell> = boardCells.filterValues(predicate).keys
 
-    override fun find(predicate: (T?) -> Boolean): Cell? {
-        return boardCells.filter { predicate(it.value) }.keys.first()
-    }
 
-    override fun any(predicate: (T?) -> Boolean): Boolean {
-        return boardCells.any { predicate(it.value) }
-    }
+    override fun find(predicate: (T?) -> Boolean): Cell? = boardCells.filter { predicate(it.value) }.keys.first()
 
-    override fun all(predicate: (T?) -> Boolean): Boolean {
-        return boardCells.all { predicate(it.value) }
-    }
+
+    override fun any(predicate: (T?) -> Boolean): Boolean = boardCells.any { predicate(it.value) }
+
+
+    override fun all(predicate: (T?) -> Boolean): Boolean = boardCells.all { predicate(it.value) }
 
 }
